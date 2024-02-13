2dfefd84bfaaffd328f231895621a466eac32909
Jean-Baptiste Loup <jb@wokine.com>
2024-02-13 12:32:13 +0100

    add wezterm.lua

---
+++ .config/.wezterm.lua
@@ -0,0 +1,21 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+config.window_background_image = './bg.jpg'
+
+-- For example, changing the color scheme:
+config.color_scheme = 'Argonaut'
+
+-- and finally, return the configuration to wezterm
+return config
+
