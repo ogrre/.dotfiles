a7b71eda1a319d7b28df616a2e26169772e15ec8
Jean-Baptiste Loup <jb@wokine.com>
2024-04-03 18:08:19 +0200

    add brew install

--- install.sh
+++ install.sh
@@ -1,1 +1,8 @@
 brew install yq
+brew install koekeishiya/formulae/skhd
+skhd --start-service
+brew install koekeishiya/formulae/yabai
+yabai --start-service
+brew tap FelixKratz/formulae
+brew install sketchybar
+brew services start sketchybar
